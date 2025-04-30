4a85a5c7a03e1b364a6509ff1402ef20266235ed
Igor Urisman <igor@Igors-MacBook-Pro.local>
2025-04-30 13:57:46 -0700

    WIP

--- stack/src/stack.rs
+++ stack/src/stack.rs
@@ -28,6 +28,34 @@ impl<E> Stack<E> {
             }
         }
     }
+
+    // fn peek(&mut self) -> Option<&E> {
+    //     self.head.as_ref().map(|node| &node.elem)
+    // }
+}
+
+
+impl <E> Drop for Stack<E> {
+
+    // Works but inefficient
+    // fn drop(&mut self) {
+    //     while let Some(node) = self.pop() {}
+    // }
+
+    // This is better
+    fn drop(&mut self) {
+        let mut curr_head = self.head.take();
+        while let Some(boxed_node) = curr_head {
+            curr_head = boxed_node.next;
+        }
+    }
+}
+
+impl <E> Iterator for Stack<E> {
+    type Item = E;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pop()
+    }
 }
 
 #[cfg(test)]
@@ -70,4 +98,41 @@ mod tests {
             assert_eq!(i-1, stack.size as i32);
         }
     }
+
+    #[test]
+    fn drop_test() {
+        let mut stack: Stack<i32> = Stack::new();
+        for i in 0..100000 {
+            stack.push(i);
+        }
+    }
+
+    #[test]
+    fn iter_test() {
+        let mut stack: Stack<String> = Stack::new();
+        for i in 0..100 {
+            stack.push(i.to_string());
+        }
+
+        for i in stack {
+            println!("{}", i);
+        }
+    }
+
+    // #[test]
+    // fn peek_test() {
+    //     let mut stack: Stack<String> = Stack::new();
+    //     assert!(stack.pop().is_none());
+    //     assert_eq!(0, stack.size);
+    //     assert!(stack.peek().is_none());
+    //     stack.push(String::from("Hello"));
+    //     assert_eq!(1, stack.size);
+    //     assert_eq!("Hello", stack.peek().unwrap());
+    //     assert_eq!("Hello", stack.peek().unwrap());
+    //     stack.push(String::from("World"));
+    //     assert_eq!(2, stack.size);
+    //     let peek = stack.peek().unwrap();
+    //     assert_eq!(peek, &stack.pop().unwrap());
+    // }
+
 }

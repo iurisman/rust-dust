7444f00787cf8309d25cc627bf9039bf3e22f838
Igor Urisman <igor@Igors-MacBook-Pro.local>
2025-05-15 12:53:08 -0700

    WIP

--- lib/src/io.rs
+++ lib/src/io.rs
@@ -1,22 +1,19 @@
 use std::fs::{File};
 use std::io::{BufRead, BufReader, Read};
 use regex::Regex;
-use std::error::Error;
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
 ///Read tokens from a file
-pub fn tokenize_file(filename: &str) -> Result<impl Iterator<Item=String>> {
-    let file = File::open(filename)?;
+pub fn tokenize_file(filename: &str) -> impl Iterator<Item=String> {
+    let file = File::open(filename).unwrap();
     from_buf_reader(file)
 }
 
-pub fn from_buf_reader(reader: impl Read) -> Result<impl Iterator<Item=String>> {
+pub fn from_buf_reader(reader: impl Read) -> impl Iterator<Item=String> {
     let regex = Regex::new(r#"[a-zA-Z0-9\d\s:]"#).unwrap();
-    let res = BufReader::new(reader).lines()
+    BufReader::new(reader).lines()
         .map(|res| res.unwrap())
         .map(move |str| str.chars().filter(|c| regex.is_match(&c.to_string())).collect::<String>())
-        .flat_map(|line| line.split_whitespace().map(String::from).collect::<Vec<String>>());
-    Ok(res)
+        .flat_map(|line| line.split_whitespace().map(String::from).collect::<Vec<String>>())
 }
 
 #[cfg(test)]
@@ -24,16 +21,15 @@ mod tests {
     use super::*;
     #[test]
     fn test_punctuation() {
-        from_buf_reader(BufReader::new("oh, la , la!".as_bytes())).unwrap()
+        from_buf_reader(BufReader::new("oh, la , la!".as_bytes()))
             .for_each(|token| {
-                println!("'{}'", token);
                 assert!(token.chars().count() > 0 && token.chars().all(|c| c.is_alphanumeric()));
             })
     }
     #[test]
     fn test_verlaine() {
         let mut token_count = 0;
-        tokenize_file("./verlaine.txt").unwrap()
+        tokenize_file("./verlaine.txt")
             .for_each(|_token| token_count += 1 );
         assert_eq!(token_count, 45);
     }

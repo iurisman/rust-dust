712f1eb20ca7597fcd2f35250a0af44104d46516
Igor Urisman <igor@MK-MBP.localdomain>
2025-06-09 10:27:07 -0700

    WIP

--- lib/src/token_with_result_v1.rs
+++ lib/src/token_with_result_v1.rs
@@ -31,25 +31,14 @@ impl Tokenizer {
 
     /// Read tokens from a file
     pub fn from_file(&self, filename: &str)
-        -> impl Iterator<Item=Result<String, TokenizerError>>
+        -> Result<impl Iterator<Item=Result<String, TokenizerError>>, TokenizerError>
     {
-        std::iter::empty()
-    //     match fs::File::open(filename) {
-    //         Ok(file) => {self.from_buf_reader(file)},
-    //         Err(error) => {
-    //             let vec: Vec<Result<String, TokenizerError>> = vec![Err(TokenizerError::from(error))];
-    //             vec.into_iter()
-    //         }
-    //     }
-    }
-
-    fn mk_error(error: io::Error) -> impl Iterator<Item=Result<String, TokenizerError>> {
-        vec![Err(TokenizerError::from(error))].into_iter()
+        Ok(self.from_buf_reader(fs::File::open(filename)?))
     }
 
     /// Read tokens from a reader
     pub fn from_buf_reader<R: io::Read>(&self, reader: R) -> impl Iterator<Item=Result<String, TokenizerError>> {
-        let foo = io::BufReader::new(reader).lines()
+        io::BufReader::new(reader).lines()
             .map(|res_line|
                 res_line.map(|line|
                     line.chars().filter(|c| (self.validator)(c)).collect::<String>()
@@ -64,8 +53,7 @@ impl Tokenizer {
                             .map(|str| Ok(String::from(str)))
                             .collect::<Vec<Result<String, _>>>()
                 }
-            );
-        foo
+            )
     }
 }
 
@@ -100,8 +88,10 @@ mod tests {
     #[test]
     fn test_verlaine() {
         let tokenizer = Tokenizer::new_with_validator(validator);
-        let (oks, errs): (Vec<Result<_,_>>, Vec<Result<_, _>>) =
-            tokenizer.from_file("./verlaine.txt").partition(|res| res.is_ok());
+        let (oks, _errs): (Vec<Result<_,_>>, Vec<Result<_, _>>) =
+            tokenizer.from_file("./verlaine.txt")
+                .unwrap()
+                .partition(|res| res.is_ok());
         assert_eq!(oks.len(), 45);
     }
 }
---
+++ lib/src/token_with_result_v2.rs
@@ -0,0 +1,105 @@
+use std::{fs, io};
+use std::io::BufRead;
+
+#[derive(Debug)]
+pub struct TokenizerError {
+    // Bad token, if any
+    pub token: Option<String>,
+    // Error message
+    pub message: String,
+    // Cause
+    //pub source: Option<Box<dyn Error>>
+}
+
+impl From<std::io::Error> for TokenizerError {
+    fn from(error: io::Error) -> Self {
+        TokenizerError {message: format!("{}", error), token: None}
+    }
+}
+
+pub struct Tokenizer {
+    validator: fn(&char) -> bool,
+}
+impl Tokenizer {
+    fn default_validator(_: &char) -> bool {true}
+    pub fn new() -> Tokenizer {
+        Tokenizer { validator: Self::default_validator}
+    }
+    pub fn new_with_validator(validator: fn(&char) -> bool) -> Tokenizer {
+        Tokenizer {validator}
+    }
+
+    /// Read tokens from a file
+    pub fn from_file(&self, filename: &str)
+        -> impl Iterator<Item=Result<String, TokenizerError>>
+    {
+        match fs::File::open(filename) {
+            Ok(file) => {self.from_buf_reader(file)},
+            Err(error) => {
+                let vec: Vec<Result<String, TokenizerError>> = vec![Err(TokenizerError::from(error))];
+                vec.into_iter()
+            }
+        }
+    }
+
+    fn mk_error(error: io::Error) -> impl Iterator<Item=Result<String, TokenizerError>> {
+        vec![Err(TokenizerError::from(error))].into_iter()
+    }
+
+    /// Read tokens from a reader
+    pub fn from_buf_reader<R: io::Read>(&self, reader: R) -> impl Iterator<Item=Result<String, TokenizerError>> {
+       io::BufReader::new(reader).lines()
+            .map(|res_line|
+                res_line.map(|line|
+                    line.chars().filter(|c| (self.validator)(c)).collect::<String>()
+                )
+            )
+            .flat_map(|res_line|
+                match res_line {
+                    Err(err) =>
+                        vec![Err(TokenizerError::from(err))],
+                    Ok(line) =>
+                        line.split_whitespace()
+                            .map(|str| Ok(String::from(str)))
+                            .collect::<Vec<Result<String, _>>>()
+                }
+            )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use regex::Regex;
+    fn validator(c: &char) -> bool {
+        // Chars we care about plus white space to split on.
+        Regex::new(r#"[^\p{Punct}]"#).unwrap().is_match(&c.to_string())
+    }
+    #[test]
+    fn test_default_validator() {
+        let tokenizer = Tokenizer::new();
+        assert_eq!(
+            tokenizer.from_buf_reader(io::BufReader::new("oh, la , la!".as_bytes()))
+                .map(|res| res.unwrap())
+                .collect::<Vec<String>>(),
+            vec!["oh,".to_string(), "la".to_string(), ",".to_string(), "la!".to_string()]
+        );
+    }
+
+    #[test]
+    fn test_punctuation() {
+        let tokenizer = Tokenizer::new_with_validator(validator);
+        tokenizer.from_buf_reader(io::BufReader::new("oh, la , la!".as_bytes()))
+            .for_each(|res| {
+                let token = res.unwrap();
+                assert!(token.chars().count() > 0 && token.chars().all(|c| c.is_alphanumeric()));
+            })
+    }
+    #[test]
+    fn test_verlaine() {
+        let tokenizer = Tokenizer::new_with_validator(validator);
+        let (oks, errs): (Vec<Result<_,_>>, Vec<Result<_, _>>) =
+            tokenizer.from_file("./verlaine.txt").partition(|res| res.is_ok());
+        assert_eq!(oks.len(), 45);
+    }
+}
